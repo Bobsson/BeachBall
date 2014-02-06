4cb55a088cd361d6b96be8c7488121eddd1c901d
Bobson <bobson@daniel-desktop-wireless.home>
2014-02-06 00:52:50 -0500

    Add logic for Ninja Ritual preference

--- BeachBall.js
+++ BeachBall.js
@@ -670,7 +670,7 @@ BeachBall.Ninja = function() {
 	//Molpy.npbONG is 0 when you can't click, and 1 when you can click
 
 	if (Molpy.ninjad == 0) {
-        if (Molpy.npbONG != 0) {
+        if (Molpy.npbONG != 0 || BeachBall.Settings['NinjaClick'].status > 0) {
             BeachBall.incoming_ONG = 0;
             if (BeachBall.Settings['BeachAutoClick'].status > 0 && Molpy.Got('Temporal Rift') == 0) {
 				Molpy.ClickBeach();
